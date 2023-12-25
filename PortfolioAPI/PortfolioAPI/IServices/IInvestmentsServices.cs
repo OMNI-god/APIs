98b406cfa54a8e1ebeec893f0f3893759c01cbd1
Rupayan Mondal <jawda09@gmail.com>
2023-12-26 00:16:40 +0530

    add services methods get

---
+++ PortfolioAPI/PortfolioAPI/IServices/IInvestmentsServices.cs
@@ -0,0 +1,11 @@
+﻿using PortfolioAPI.Models;
+
+namespace PortfolioAPI.IServices
+{
+    public interface IInvestmentsServices
+    {
+        IEnumerable<Investment> GetInvestments();
+        Investment GetInvestment(int id);
+
+    }
+}
---
+++ PortfolioAPI/PortfolioAPI/Services/InvestmentsServices.cs
@@ -0,0 +1,18 @@
+﻿using PortfolioAPI.IServices;
+using PortfolioAPI.Models;
+
+namespace PortfolioAPI.Services
+{
+    public class InvestmentsServices : IInvestmentsServices
+    {
+        public Investment GetInvestment(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Investment> GetInvestments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
